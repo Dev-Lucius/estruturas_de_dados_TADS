f78e0d368ab6c3dede21fbb1a2473eacfab3a8bc
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-08-18 20:03:38 -0300

    Add year of release to Filme struct and update output
    Added a year of release field and updated it in the output.

--- Aula3/Filme.c
+++ Aula3/Filme.c
@@ -1,4 +1,8 @@
 // Crie uma Struct para Armazenar Dados de um Filme;
+// Defina duas variáveis para armazenar os dados de um Filme
+// Mostre os dados armazenados do Filme
+// Troque o ano de lançamento do filmes
+// Mostre os Dados Atualizados
 
 #include <stdio.h>
 #include <string.h>
@@ -9,6 +13,7 @@ typedef struct{
     char diretor[50];
     float avaliacao;
     double arrecadacao;
+    int anoLançamento;
 } Filme;
 
 int main(){
@@ -20,10 +25,16 @@ int main(){
     strcpy(f1.diretor, "Destin Daniel Cretton");
     f1.avaliacao = 9.87;
     f1.arrecadacao = 26000000.90;
+    f1.anoLançamento = 2026;
 
     printf("Codigo = %d \n", f1.cod);
     printf("Nome Filme = %s \n", f1.nome);
     printf("Diretor = %s \n", f1.diretor);
     printf("Avaliação = %.2f \n", f1.avaliacao);
     printf("Arrecadação = %.5f \n", f1.arrecadacao);
+    printf("Ano de Lançamento = %d \n", f1.anoLançamento);
+
+    printf("Alterando Ano de Lançamento \n");
+    f1.anoLançamento = 2025;
+    printf("Ano de Lançamento = %d \n", f1.anoLançamento);
 }
