7e6c41e0a6683df763a5ccbe43e36c2e3b26ef02
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-08-18 20:03:27 -0300

    Implement car and date structs with display functionality
    Added a struct for storing car data and a struct for date. Included functionality to display and update car manufacturing date.

--- Aula3/Carro.c
+++ Aula3/Carro.c
@@ -1,4 +1,8 @@
 // Crie uma Struct para Armazenar Dados de um Carro;
+// Defina duas variáveis para armazenar os dados de um Carro
+// Mostre os dados armazenados dos carros
+// Troque o ano de fabricação dos carros
+// Mostre os Dados Atualizados
 
 #include <stdio.h>
 #include <string.h>
@@ -11,9 +15,19 @@ typedef struct{
     double quilometragem;
 } Carro;
 
+typedef struct {
+    int dia;
+    int mes;
+    int ano
+} Data;
+
 int main(){
 
     Carro c1;
+    Data d1;
+    d1.dia = 25;
+    d1.mes = 5;
+    d1.ano = 2019;
 
     c1.cod = 1;
     strcpy(c1.placa, "AFKG-(4582)");
@@ -26,4 +40,17 @@ int main(){
     printf("Marca = %s \n", c1.marca);
     printf("Potência = %f \n", c1.potencia);
     printf("Quilometragem = %f \n", c1.quilometragem);
+    printf("Ano de Fabricação = %d \n", d1.ano);
+    printf("Dia de Fabricação = %d \n", d1.dia);
+    printf("Mes de Fabricação = %d \n", d1.mes);
+
+    // Trocando Ano de Fabricação
+    d1.dia = 15;
+    d1.mes = 9;
+    d1.ano = 2006;
+
+    printf("Após a Troca \n");
+    printf("Ano de Fabricação = %d \n", d1.ano);
+    printf("Dia de Fabricação = %d \n", d1.dia);
+    printf("Mes de Fabricação = %d \n", d1.mes);
 }
